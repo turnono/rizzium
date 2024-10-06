c80d1369ea804c1206c75890d6cacc59c49d36cf
Abdullah <abdullah.abrahams@4-sure.net>
2024-10-06 20:10:44 +0200

    chore: ionic

--- apps/rizzpos/angular/src/app/app.config.ts
+++ apps/rizzpos/angular/src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideAnimationsAsync(),
+    provideRouter(appRoutes), provideAnimationsAsync(), provideIonicAngular({}),
   ],
 };
