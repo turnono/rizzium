819420c2dea77e8b3cbdc3b43d56513782ac05a6
Abdullah <abdullah.abrahams@4-sure.net>
2024-11-25 08:23:20 +0200

    hjghjk

--- apps/rizzpos/angular/src/app/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCDICBhm6P9MvAKPqvox6SVbWX7Rvm5k20",
-  authDomain: "rizzpos.firebaseapp.com",
-  projectId: "rizzpos",
-  storageBucket: "rizzpos.appspot.com",
-  messagingSenderId: "621517733133",
-  appId: "1:621517733133:web:43d4a7086737df56aa8444",
-  measurementId: "G-Y5CETJN7PV"
-};
