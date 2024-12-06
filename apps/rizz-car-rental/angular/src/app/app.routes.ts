e95a4e1a5136f8d1e05a5d378b4e80e52449f790
Abdullah <abdullah.abrahams@4-sure.net>
2024-12-06 15:11:14 +0700

    refactor: remove rizz-bnb and rizz-car-rental applications and their associated configurations

--- apps/rizz-car-rental/angular/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [
-  {
-    path: '',
-    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
-  },
-];
