0b82a2484ae1b77008b84a1e7f88074ce5068a70
Abdullah <abdullah.abrahams@4-sure.net>
2024-12-19 00:06:30 +0200

    chore: include 'islamiccourt' project

---
+++ apps/islamiccourt/functions/user/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'islamiccourt-functions-user',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/apps/islamiccourt/functions/user',
+};
