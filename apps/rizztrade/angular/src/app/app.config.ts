67e8b2cf2f37cea75aa674fbaec6f46c8a44803a
Abdullah <abdullah.abrahams@4-sure.net>
2025-01-02 22:13:37 +0200

    feat: implement landing page and enhance app configuration
    - Added a new standalone LandingComponent with a video background and animated text.
    - Updated app routing to load the LandingComponent for the root path.
    - Enhanced app configuration by integrating Firebase Storage support.
    - Refined project.json for better asset and style management.
    - Introduced new assets including various favicon sizes and a video for the landing page.
    - Updated Firebase storage rules to allow read access for a specific video file.
    
    These changes improve the user experience with a dynamic landing page and streamline the app's configuration for Firebase integration.

--- apps/rizztrade/angular/src/app/app.config.ts
+++ apps/rizztrade/angular/src/app/app.config.ts
@@ -1,16 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, RouteReuseStrategy } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  IonicRouteStrategy,
-  provideIonicAngular,
-} from '@ionic/angular/standalone';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { firebaseConfig } from './firebase-config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { arrowDownOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+
+// Register Ionicons
+addIcons({
+  'arrow-down-outline': arrowDownOutline,
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +26,7 @@ export const appConfig: ApplicationConfig = {
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
     provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
   ],
