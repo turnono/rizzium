682a492a5670e7bbbd92e6b4c6fded988e0f51ae
Abdullah <abdullah.abrahams@4-sure.net>
2025-01-19 23:53:11 +0200

    chore: remove Cypress end-to-end test files and configurations for multiple applications
    - Deleted Cypress configuration files, ESLint configurations, and TypeScript configurations for various applications including finescan-e2e, flowyclarity-e2e, islamiccourt-e2e, rizz-social-e2e, rizzpos-e2e, and rizztrade-e2e.
    - Removed associated end-to-end test files and support files, streamlining the project structure and eliminating unused test cases.
    - This cleanup enhances maintainability and reduces clutter in the codebase, ensuring a more focused development environment.

--- apps/finescan-e2e/src/e2e/app.cy.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('finescan-e2e', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains(/Welcome/);
-  });
-});
